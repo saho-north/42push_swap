8b99c1cc4ed40ebbdf29811f66d5887d62fa47bd
saho-north <SahoKitahara@my.uopeople.edu>
2023-07-09 04:06:11 +0900

    solved double free problem in lst_clear.c

--- lst_clear.c
+++ lst_clear.c
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 03:03:19 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/07/09 04:01:49 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/07/09 04:05:36 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,21 @@ void	lst_clear(t_list **lst)
 {
 	t_list	*tmp;
 	t_list	*next;
+	bool	is_dummy;
 
 	if (!lst)
 		return ;
-	tmp = *lst;
-	while (tmp)
+	is_dummy = false;
+	tmp = (*lst)->next;
+	while (tmp && !is_dummy)
 	{
-		next = tmp->next;
-		free(tmp);
-		tmp = next;
 		if (tmp->is_dummy)
-			break ;
+			is_dummy = true;
+		else
+			next = tmp->next;
+		free(tmp);
+		if (!is_dummy)
+			tmp = next;
 	}
 	*lst = NULL;
 }
