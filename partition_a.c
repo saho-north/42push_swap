5142a488dc2d76769f309a58c34b9e518911509e
Saho Kitahara <sakitaha@c1r9s9.42tokyo.jp>
2023-08-02 11:03:03 +0900

    norminette OK

--- partition_a.c
+++ partition_a.c
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 04:20:21 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/08/02 10:08:56 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/08/02 11:00:22 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,58 @@ static void	sort_top_3_a(t_stack *a)
 	}
 }
 
-void	partition_a(t_stack *a, t_stack *b, size_t size, size_t max_id)
+static void	do_rrr(t_stack *a, t_stack *b, size_t count)
 {
 	size_t	i;
-	size_t	pivot_left;
-	size_t	pivot_right;
-	size_t	push_count;
 
+	i = 0;
+	while (i < count)
+	{
+		rrr(a, b);
+		i++;
+	}
+}
+
+static void	call_next(t_stack *a, t_stack *b, size_t size, size_t max_id)
+{
+	do_rrr(a, b, size / 3);
+	partition_a(a, b, size / 3, max_id);
+	partition_b(a, b, size / 3, max_id - (size / 3) * 2 + 1);
+	partition_b(a, b, size - (size / 3) * 2, max_id + 1 - size);
+}
+
+static void	partition_a_helper(t_stack *a, t_stack *b, size_t size,
+		size_t max_id)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (a->guard->next->index > max_id - size / 3)
+			ra(a);
+		else if (a->guard->next->index > max_id - (size / 3) * 2)
+		{
+			pb(a, b);
+			if (i + 1 < size && a->guard->next->index > max_id - size / 3)
+			{
+				rr(a, b);
+				i++;
+			}
+			else
+				rb(b);
+		}
+		else
+		{
+			pb(a, b);
+		}
+		i++;
+	}
+	call_next(a, b, size, max_id);
+}
+
+void	partition_a(t_stack *a, t_stack *b, size_t size, size_t max_id)
+{
 	if (size < 2 || is_sorted(a, size))
 	{
 		return ;
@@ -57,43 +102,5 @@ void	partition_a(t_stack *a, t_stack *b, size_t size, size_t max_id)
 		sort_top_3_a(a);
 		return ;
 	}
-	pivot_left = max_id - (size / 3) * 2;
-	pivot_right = max_id - size / 3;
-	push_count = 0;
-	i = 0;
-	while (i < size)
-	{
-		if (a->guard->next->index > pivot_right)
-		{
-			ra(a);
-		}
-		else if (a->guard->next->index > pivot_left)
-		{
-			pb(a, b);
-			if (i + 1 < size && a->guard->next->index > pivot_right)
-			{
-				rr(a, b);
-				i++;
-			}
-			else
-			{
-				rb(b);
-			}
-		}
-		else
-		{
-			pb(a, b);
-			push_count++;
-		}
-		i++;
-	}
-	i = 0;
-	while (i < (size - push_count) / 2)
-	{
-		rrr(a, b);
-		i++;
-	}
-	partition_a(a, b, (size - push_count) / 2, max_id);
-	partition_b(a, b, (size - push_count) / 2, pivot_left + 1);
-	partition_b(a, b, push_count, max_id + 1 - size);
+	partition_a_helper(a, b, size, max_id);
 }
